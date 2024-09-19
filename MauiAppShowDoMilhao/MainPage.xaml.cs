756ad3a8148964e9643b9674103800c1e02047e0
Usuario <Usuario@CPD03-M06>
2024-09-19 14:36:22 -0300

    nao finalizado

--- MauiAppShowDoMilhao/MainPage.xaml.cs
+++ MauiAppShowDoMilhao/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MauiAppShowDoMilhao.Models;
+using Plugin.Maui.Audio;
 
 namespace MauiAppShowDoMilhao
 {
@@ -13,14 +14,70 @@ namespace MauiAppShowDoMilhao
             InitializeComponent();
 
             this.BindingContext = App.getRandomPerguntaFacil();
+
             lbl_nivel.Text = "Fácil";
             lbl_premio.Text= premio.ToString("C");
             lbl_pergunta_numero.Text= pergunta_count.ToString();
+            Stream track = FileSystem.OpenAppPackageFileAsync("0.mp3").Result;
+            AudioManager.Current.CreatePlayer(track).Play;
         }
 
-        
+        private void toca_som()
+        {
+            string track = "";
 
-       
+            switch (pergunta_count)
+            {
+                case 1:
+                    track = "1000.wav";
+                    break;
+                case 2:
+                    track = "2000.wav";
+                    break;
+                case 3:
+                    track = "3000.wav";
+                    break;
+                case 4:
+                    track = "4000.wav";
+                    break;
+                case 5:
+                    track = "5000.wav";
+                    break;
+                case 6:
+                    track = "10000.wav";
+                    break;
+                case 7:
+                    track = "20000.wav";
+                    break;
+                case 8:
+                    track = "30000.wav";
+                    break;
+                case 9:
+                    track = "40000.wav";
+                    break;
+                case 10:
+                    track = "50000.wav";
+                    break;
+                case 11:
+                    track = "100000.wav";
+                    break;
+                case 12:
+                    track = "200000.wav";
+                    break;
+                case 13:
+                    track = "300000.wav";
+                    break;
+                case 14:
+                    track = "400000.wav";
+                    break;
+                case 15:
+                    track = "500000.wav";
+                    break;
+
+            }
+
+            AudioManager.Current.CreatePlayer(FileSystem.OpenAppPackageFileAsync(track).Result).Play();
+        }
        
         void avanca_pergunta()
         {
